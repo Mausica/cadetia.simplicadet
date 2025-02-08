06688c97e98be229826c6c688a8ef7e6957583ba
Marius <mausica.dev@gmail.com>
2025-02-09 00:40:55 +0200

    light.bottom

--- simplicadet.app/app/src/main/java/com/cadetia/simplicadet/activities/Home.java
+++ simplicadet.app/app/src/main/java/com/cadetia/simplicadet/activities/Home.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
@@ -16,6 +17,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
+import androidx.core.view.WindowCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -46,13 +48,23 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         super.onCreate(savedInstanceState);
 
         Window window = getWindow();
-        window.setStatusBarColor(getResources().getColor(R.color.transparent));
+        window.setStatusBarColor(getResources().getColor(R.color.focus));
+
+        // Use WindowCompat to set fitsSystemWindows to false
+        View decorView = getWindow().getDecorView();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // For Android 11 and above
+            WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        } else {
+            // For versions below Android 11
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
 
         com.cadetia.simplicadet.databinding.ActivityHomeBinding binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        navView.setBackgroundResource(R.drawable.gradient_bottom_light);
+        //navView.setBackgroundResource(R.drawable.gradient_bottom_light);
         navView.setItemRippleColor(ColorStateList.valueOf(Color.TRANSPARENT));
 
         int[][] states = new int[][] {new int[] { android.R.attr.state_selected }, new int[] {}};
@@ -112,7 +124,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         int itemId = menuItem.getItemId();
-        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+        //BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_home);
 
         if (itemId == R.id.drawer_logout) {
